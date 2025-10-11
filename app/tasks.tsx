bb1a904a11feffc2b25a4241ec5d265beea63ae1
mekuanint <venapp22@gmail.com>
2025-10-11 01:41:16 -1200

    updating 1

---
+++ app/tasks.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+export default function TasksScreen() {
+    return (
+        <View 
+        style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "lightblue",
+        }}
+        >
+            
+            <Text> 📝 VenMeku  screen!</Text></View>
+    );
+}
